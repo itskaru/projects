f4144addb6bc40ac7e393f1e9df5ac4c04cd6170
Robert <84203515+itskaru@users.noreply.github.com>
2024-03-25 14:02:55 +0100

    Delete proyects directory

--- proyects/helloworld.js
+++
@@ -1,9 +0,0 @@
-function suma(num1,num2){
-    return num1+num2;
-}
-
-let num1 = 5;
-let num2 = 4;
-let r = suma(num1,num2);
-document.writeln("Hola Mundo")
-document.writeln("El resultado de la suma es: ", r);
